e775f3034dd3d18c7ecdfa6670e14cd207e82f85
vit718 <vitfrei2024@gmail.com>
2023-08-22 11:03:04 +0600

    first commit without npm

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    plugins: [
+        require('postcss-preset-env'),
+    ],
+};
+
+// "sass": "sass assets/scss/style.scss assets/styles/style.css -w"
+// "postcss": "postcss assets/styles/style.css -o assets/styles/style.css -w"
